8ea21ae7a72155bddb462fc13e0a32d2b07552e6
NONOJAM <90127842+pearljamcxy@users.noreply.github.com>
2025-11-07 11:09:50 +0900

    commit

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello GitHub C#!");
+        Console.WriteLine("welcome to the PUZZLES!");
+        Console.WriteLine("this is the number between 1 to 100, can you guess?");
+        
+        var random = new Random();
+        
     }
 }
